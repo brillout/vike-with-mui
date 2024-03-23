79a4e207576a270fd7e3d9ff3412b773cf48f956
Romuald Brillout <git@brillout.com>
2024-03-23 10:02:48 +0100

    fix with `vite-plugin-cjs-interop`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import ssr from "vike/plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { cjsInterop } from "vite-plugin-cjs-interop";
 
 export default defineConfig({
-  plugins: [react({}), ssr({})],
+  plugins: [
+    react({}),
+    ssr({}),
+    cjsInterop({
+      dependencies: [
+        // Add problematic npm package here
+        "@mui/material/**",
+      ]
+    })
+  ],
 });
